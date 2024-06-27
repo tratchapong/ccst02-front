e8cd6f19151ea813885d407449580cfd8b845fa9
tratchapong <tratchapong@gmail.com>
2024-06-27 21:21:24 +0700

    conditional route by user in AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,37 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Header from "../layouts/Header";
 import LoginForm from "../pages/LoginForm";
 import RegisterForm from "../pages/RegisterForm";
+import useAuth from "../hooks/useAuth";
+
+const teacherRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Header/>
+      <Outlet />
+    </>,
+    children: [
+      {index: true, element : <p>Teacher Home</p>},
+      {path: '/new', element : <p>Add new Homework</p>},
+      {path: '*', element : <p>Page not found</p>},
+    ]
+  }
+])
+
+const studentRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <Header/>
+      <Outlet />
+    </>,
+    children: [
+      {index: true, element : <p>Student Home</p>},
+      {path: '/profile', element : <p>Student Profile</p>},
+      {path: '*', element : <p>Page not found</p>},
+    ]
+  }
+])
 
 const guestRouter = createBrowserRouter([
   {
@@ -15,10 +46,18 @@ const guestRouter = createBrowserRouter([
     children: [
       { index: true, element: <LoginForm /> },
       { path: "register", element: <RegisterForm /> },
+      {path: '*', element : <p>Page not found</p>},
     ],
   },
 ]);
 
+const finalRouter = (user) => {
+  if(!user) { return guestRouter}
+  if(user.t_code) { return teacherRouter}
+  return studentRouter
+}
+
 export default function AppRouter() {
-  return <RouterProvider router={guestRouter} />;
+  const {user} = useAuth()
+  return <RouterProvider router={finalRouter(user)} />;
 }
